b63ae64b407fa5e3022f0180880e4fa811ce8898
Alberto Melendez <amelendez.es@gmail.com>
2025-07-06 12:39:01 +0200

    Renamed countResolvedApplicationConflictsQuery

--- src/services/query/countResolvedApplicationConflictsQuery.js
+++ src/services/query/countResolvedApplicationConflictsQuery.js
@@ -5,7 +5,7 @@ const DB_VERSION = 9;
 
 const getDbPromise = () => openDB(DB_NAME, DB_VERSION);
 
-export const countApplicationConflictsQuery = async () => {
+export const countResolvedApplicationConflictsQuery = async () => {
   const db = await getDbPromise();
   const tx = db.transaction('new_env_conflicts', 'readonly');
   const store = tx.objectStore('new_env_conflicts');
