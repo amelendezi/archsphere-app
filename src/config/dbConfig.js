45cf35c73f33287982bcb7d950cb5706d85108cd
Alberto Melendez <amelendez.es@gmail.com>
2025-07-06 16:24:32 +0200

    Updated store names

--- src/config/dbConfig.js
+++ src/config/dbConfig.js
@@ -1,6 +1,6 @@
 export const DB_NAME = 'ArchSphereDB';
 export const DB_VERSION = 10;
-export const SETUP_ENV_APPLICATIONS_STORE_NAME = 'env_applications';
-export const SETUP_NEW_APPLICATIONS_STORE_NAME = 'new_applications';
-export const SETUP_CONFLICTS_STORE_NAME = 'new_env_conflicts';
-export const SETUP_ADDED_APPLICATIONS_STORE_NAME = 'rec_new_applications';
+export const SETUP_ENV_APPLICATIONS_STORE_NAME = 'setup_env_applications';
+export const SETUP_NEW_APPLICATIONS_STORE_NAME = 'setup_new_applications';
+export const SETUP_CONFLICTS_STORE_NAME = 'setup_conflicts';
+export const SETUP_ADDED_APPLICATIONS_STORE_NAME = 'setup_added_applications';
