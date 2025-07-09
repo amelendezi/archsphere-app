8377736e436ebd732def4b513f204a2fd8a117a8
Alberto Melendez <amelendez.es@gmail.com>
2025-07-09 22:07:06 +0200

    Changed the styles.

--- src/components/SetupWizardComponent/Reconciliation/Reconciliation.js
+++ src/components/SetupWizardComponent/Reconciliation/Reconciliation.js
@@ -46,144 +46,142 @@ function Reconciliation({ onBack, onClose, setSelectedFile, setSelectedNewApplic
   }, [conflictCount, resolvedConflictCount]);
   return (
     <div style={{ backgroundColor: 'white', padding: '20px', borderRadius: '8px' }}>
-      <h2 style={{ color: 'black' }}>Reconcile New Applications</h2>      
-      <table style={{ width: '100%', borderCollapse: 'collapse', border: '1px solid #ddd', margin: '0 auto' }}>
-        <thead>
-          <tr>
-            <th style={{ border: '1px solid #ddd', padding: '8px', fontWeight: 'normal' }}>
-              <div style={{ display: 'flex', alignItems: 'center', gap: '5px', justifyContent: 'center' }}>
-                <div
-                  style={{ position: 'relative', display: 'inline-block', marginRight: '5px' }}
-                  onMouseEnter={() => setShowTooltipNewApplications(true)}
-                  onMouseLeave={() => setShowTooltipNewApplications(false)}
-                >
-                  <span style={{ display: 'inline-block', width: '16px', height: '16px', borderRadius: '50%', border: '1px solid #4285F4', textAlign: 'center', lineHeight: '14px', fontSize: '12px', fontWeight: 'bold', cursor: 'help', color: '#4285F4' }}>i</span>
-                  <span style={{
-                    width: '250px',
-                    backgroundColor: '#E0E0E0',
-                    color: '#000',
-                    textAlign: 'left',
-                    borderRadius: '6px',
-                    padding: '10px',
-                    position: 'absolute',
-                    zIndex: '1',
-                    bottom: '125%',
-                    left: '50%',
-                    marginLeft: '-125px',
-                    opacity: showTooltipNewApplications ? '1' : '0',
-                    visibility: showTooltipNewApplications ? 'visible' : 'hidden',
-                    transition: 'opacity 0.375s',
-                    border: '1px solid #E0E0E0',
-                    boxShadow: '0 2px 5px rgba(0,0,0,0.3)',
-                    fontSize: '0.8em'
-                  }}>
-                    Number of new applications that can be added to the environment applications, that posses no conflict with any existing applications.
-                  </span>
-                </div>
-                New Applications
-              </div>
-            </th>
-            <th style={{ border: '1px solid #ddd', padding: '8px', fontWeight: 'normal' }}>
-              <div style={{ display: 'flex', alignItems: 'center', gap: '5px', justifyContent: 'center' }}>
-                <div
-                  style={{ position: 'relative', display: 'inline-block', marginRight: '5px' }}
-                  onMouseEnter={() => setShowTooltipConflicts(true)}
-                  onMouseLeave={() => setShowTooltipConflicts(false)}
-                >
-                  <span style={{ display: 'inline-block', width: '16px', height: '16px', borderRadius: '50%', border: '1px solid #4285F4', textAlign: 'center', lineHeight: '14px', fontSize: '12px', fontWeight: 'bold', cursor: 'help', color: '#4285F4' }}>i</span>
-                  <span style={{
-                    width: '250px',
-                    backgroundColor: '#E0E0E0',
-                    color: '#000',
-                    textAlign: 'left',
-                    borderRadius: '6px',
-                    padding: '10px',
-                    position: 'absolute',
-                    zIndex: '1',
-                    bottom: '125%',
-                    left: '50%',
-                    marginLeft: '-125px',
-                    opacity: showTooltipConflicts ? '1' : '0',
-                    visibility: showTooltipConflicts ? 'visible' : 'hidden',
-                    transition: 'opacity 0.375s',
-                    border: '1px solid #E0E0E0',
-                    boxShadow: '0 2px 5px rgba(0,0,0,0.3)',
-                    fontSize: '0.8em'
-                  }}>
-                    Number of applications that posses conflicting values with environment applications.
-                  </span>
-                </div>
-                Conflicts
-              </div>
-            </th>
-            <th style={{ border: '1px solid #ddd', padding: '8px', fontWeight: 'normal' }}>
-              <div style={{ display: 'flex', alignItems: 'center', gap: '5px', justifyContent: 'center' }}>
-                <div
-                  style={{ position: 'relative', display: 'inline-block', marginRight: '5px' }}
-                  onMouseEnter={() => setShowTooltipEnvironmentApplications(true)}
-                  onMouseLeave={() => setShowTooltipEnvironmentApplications(false)}
-                >
-                  <span style={{ display: 'inline-block', width: '16px', height: '16px', borderRadius: '50%', border: '1px solid #4285F4', textAlign: 'center', lineHeight: '14px', fontSize: '12px', fontWeight: 'bold', cursor: 'help', color: '#4285F4' }}>i</span>
-                  <span style={{
-                    width: '250px',
-                    backgroundColor: '#E0E0E0',
-                    color: '#000',
-                    textAlign: 'left',
-                    borderRadius: '6px',
-                    padding: '10px',
-                    position: 'absolute',
-                    zIndex: '1',
-                    bottom: '125%',
-                    left: '50%',
-                    marginLeft: '-125px',
-                    opacity: showTooltipEnvironmentApplications ? '1' : '0',
-                    visibility: showTooltipEnvironmentApplications ? 'visible' : 'hidden',
-                    transition: 'opacity 0.375s',
-                    border: '1px solid #E0E0E0',
-                    boxShadow: '0 2px 5px rgba(0,0,0,0.3)',
-                    fontSize: '0.8em'
-                  }}>
-                    Number of applications currently in the environment.
-                  </span>
-                </div>
-                Environment Applications
-              </div>
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td style={{ border: '1px solid #ddd', padding: '8px' }}>{newApplicationsCount}</td>
-            <td style={{ border: '1px solid #ddd', padding: '8px' }}>{conflictCount}</td>
-            <td style={{ border: '1px solid #ddd', padding: '8px' }}>{totalApplicationsCount}</td>
-          </tr>
-          <tr>
-            <td style={{ border: '1px solid #ddd', padding: '8px' }}>
-              {showUndoButton ? (
-                <button className="reconciliationButtonPrimary" style={{ backgroundColor: 'grey' }} onClick={() => onUndoAddAllNewApplications(getStoreCount, setTotalApplicationsCount, setNewApplicationsCount, setShowUndoButton)}>
-                  Undo Add All
-                </button>
-              ) : (
-                <button className="reconciliationButtonPrimary" onClick={() => onAddAllNewApplications(getStoreCount, setTotalApplicationsCount, setNewApplicationsCount, setShowUndoButton)}>
-                  Add All
-                </button>
-              )}
-            </td>
-            <td style={{ border: '1px solid #ddd', padding: '8px' }}>
-              {showUndoAssumeAllButton ? (
-                <button className="reconciliationButtonPrimary" style={{ backgroundColor: 'grey' }} onClick={() => onUndoAssumeAllConflicts(getStoreCount, setConflictCount, setTotalApplicationsCount, setShowUndoAssumeAllButton, setRefreshConflictsTable)}>
-                  Undo Assume All
-                </button>
-              ) : (
-                <button className="reconciliationButtonPrimary" onClick={() => onAssumeAllConflicts(getStoreCount, setConflictCount, setTotalApplicationsCount, setShowUndoAssumeAllButton, setRefreshConflictsTable)}>
-                  Assume All
-                </button>
-              )}
-            </td>
-            <td style={{ border: '1px solid #ddd', padding: '8px' }}></td>
-          </tr>
-        </tbody>
-      </table>      
+      <h2 style={{ color: 'black' }}>Reconcile New Applications</h2>
+      <div style={{ display: 'flex', justifyContent: 'space-around', textAlign: 'center', margin: '20px 0' }}>
+        
+        {/* New Applications Tile */}
+        <div style={{ border: '1px solid #ddd', padding: '20px', borderRadius: '8px', width: '30%' }}>
+          <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', gap: '5px' }}>
+            <div
+              style={{ position: 'relative', display: 'inline-block' }}
+              onMouseEnter={() => setShowTooltipNewApplications(true)}
+              onMouseLeave={() => setShowTooltipNewApplications(false)}
+            >
+              <span style={{ display: 'inline-block', width: '16px', height: '16px', borderRadius: '50%', border: '1px solid #4285F4', textAlign: 'center', lineHeight: '14px', fontSize: '12px', fontWeight: 'bold', cursor: 'help', color: '#4285F4' }}>i</span>
+              <span style={{
+                width: '250px',
+                backgroundColor: '#E0E0E0',
+                color: '#000',
+                textAlign: 'left',
+                borderRadius: '6px',
+                padding: '10px',
+                position: 'absolute',
+                zIndex: '1',
+                bottom: '125%',
+                left: '50%',
+                marginLeft: '-125px',
+                opacity: showTooltipNewApplications ? '1' : '0',
+                visibility: showTooltipNewApplications ? 'visible' : 'hidden',
+                transition: 'opacity 0.375s',
+                border: '1px solid #E0E0E0',
+                boxShadow: '0 2px 5px rgba(0,0,0,0.3)',
+                fontSize: '0.8em'
+              }}>
+                Number of new applications that can be added to the environment applications, that posses no conflict with any existing applications.
+              </span>
+            </div>
+            <h3 style={{ fontWeight: 'normal', margin: 0 }}>New Applications</h3>
+          </div>
+          <p style={{ fontSize: '2.5em', margin: '10px 0' }}>{newApplicationsCount}</p>
+          <div>
+            {showUndoButton ? (
+              <button className="reconciliationButtonPrimary" style={{ backgroundColor: 'grey' }} onClick={() => onUndoAddAllNewApplications(getStoreCount, setTotalApplicationsCount, setNewApplicationsCount, setShowUndoButton)}>
+                Undo Add All
+              </button>
+            ) : (
+              <button className="reconciliationButtonPrimary" onClick={() => onAddAllNewApplications(getStoreCount, setTotalApplicationsCount, setNewApplicationsCount, setShowUndoButton)}>
+                Add All
+              </button>
+            )}
+          </div>
+        </div>
+
+        {/* Conflicts Tile */}
+        <div style={{ border: '1px solid #ddd', padding: '20px', borderRadius: '8px', width: '30%' }}>
+          <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', gap: '5px' }}>
+            <div
+              style={{ position: 'relative', display: 'inline-block' }}
+              onMouseEnter={() => setShowTooltipConflicts(true)}
+              onMouseLeave={() => setShowTooltipConflicts(false)}
+            >
+              <span style={{ display: 'inline-block', width: '16px', height: '16px', borderRadius: '50%', border: '1px solid #4285F4', textAlign: 'center', lineHeight: '14px', fontSize: '12px', fontWeight: 'bold', cursor: 'help', color: '#4285F4' }}>i</span>
+              <span style={{
+                width: '250px',
+                backgroundColor: '#E0E0E0',
+                color: '#000',
+                textAlign: 'left',
+                borderRadius: '6px',
+                padding: '10px',
+                position: 'absolute',
+                zIndex: '1',
+                bottom: '125%',
+                left: '50%',
+                marginLeft: '-125px',
+                opacity: showTooltipConflicts ? '1' : '0',
+                visibility: showTooltipConflicts ? 'visible' : 'hidden',
+                transition: 'opacity 0.375s',
+                border: '1px solid #E0E0E0',
+                boxShadow: '0 2px 5px rgba(0,0,0,0.3)',
+                fontSize: '0.8em'
+              }}>
+                Number of applications that posses conflicting values with environment applications.
+              </span>
+            </div>
+            <h3 style={{ fontWeight: 'normal', margin: 0 }}>Conflicts</h3>
+          </div>
+          <p style={{ fontSize: '2.5em', margin: '10px 0' }}>{conflictCount}</p>
+          <div>
+            {showUndoAssumeAllButton ? (
+              <button className="reconciliationButtonPrimary" style={{ backgroundColor: 'grey' }} onClick={() => onUndoAssumeAllConflicts(getStoreCount, setConflictCount, setTotalApplicationsCount, setShowUndoAssumeAllButton, setRefreshConflictsTable)}>
+                Undo Assume All
+              </button>
+            ) : (
+              <button className="reconciliationButtonPrimary" onClick={() => onAssumeAllConflicts(getStoreCount, setConflictCount, setTotalApplicationsCount, setShowUndoAssumeAllButton, setRefreshConflictsTable)}>
+                Assume All
+              </button>
+            )}
+          </div>
+        </div>
+
+        {/* Environment Applications Tile */}
+        <div style={{ border: '1px solid #ddd', padding: '20px', borderRadius: '8px', width: '30%' }}>
+          <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', gap: '5px' }}>
+            <div
+              style={{ position: 'relative', display: 'inline-block' }}
+              onMouseEnter={() => setShowTooltipEnvironmentApplications(true)}
+              onMouseLeave={() => setShowTooltipEnvironmentApplications(false)}
+            >
+              <span style={{ display: 'inline-block', width: '16px', height: '16px', borderRadius: '50%', border: '1px solid #4285F4', textAlign: 'center', lineHeight: '14px', fontSize: '12px', fontWeight: 'bold', cursor: 'help', color: '#4285F4' }}>i</span>
+              <span style={{
+                width: '250px',
+                backgroundColor: '#E0E0E0',
+                color: '#000',
+                textAlign: 'left',
+                borderRadius: '6px',
+                padding: '10px',
+                position: 'absolute',
+                zIndex: '1',
+                bottom: '125%',
+                left: '50%',
+                marginLeft: '-125px',
+                opacity: showTooltipEnvironmentApplications ? '1' : '0',
+                visibility: showTooltipEnvironmentApplications ? 'visible' : 'hidden',
+                transition: 'opacity 0.375s',
+                border: '1px solid #E0E0E0',
+                boxShadow: '0 2px 5px rgba(0,0,0,0.3)',
+                fontSize: '0.8em'
+              }}>
+                Number of applications currently in the environment.
+              </span>
+            </div>
+            <h3 style={{ fontWeight: 'normal', margin: 0 }}>Environment Applications</h3>
+          </div>
+          <p style={{ fontSize: '2.5em', margin: '10px 0' }}>{totalApplicationsCount}</p>
+          <div style={{ height: '30px' }}>{/* Placeholder for spacing, to match other tiles */}</div>
+        </div>
+
+      </div>
+      
       <div style={{ width: '100%', margin: '0 auto' }}>
         <ConflictsTable refreshTrigger={refreshConflictsTable} setRefreshConflictsTable={setRefreshConflictsTable} />
       </div>
