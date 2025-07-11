9677f41559cb6af60cc9a18aea85c8c8a33ffa07
Alberto Melendez <amelendez.es@gmail.com>
2025-07-11 10:43:18 +0200

    Layout

--- src/pages/AppListsPage.js
+++ src/pages/AppListsPage.js
@@ -1,20 +1,56 @@
 
 import React from 'react';
 
-const AppLists = () => {
+const AppListsPage = () => {
   return (
     <div style={{
-      backgroundColor: 'white',
-      minHeight: '100vh',
       display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center',
-      fontSize: '2em',
+      height: '100vh',
+      backgroundColor: 'white',
       color: 'black'
     }}>
-      <h1>Hello World for Lists</h1>
+      {/* Left Menu Pane */}
+      <div style={{
+        width: '20%',
+        border: '1px solid red',
+        padding: '10px',
+        boxSizing: 'border-box'
+      }}>
+        <h2>Left Menu Pane</h2>
+      </div>
+
+      {/* Main Content Wrapper */}
+      <div style={{
+        width: '80%',
+        display: 'flex',
+        flexDirection: 'column',
+        border: '1px solid red',
+        padding: '10px',
+        boxSizing: 'border-box'
+      }}>
+        {/* Header Menu Pane */}
+        <div style={{
+          height: '10%',
+          border: '1px solid red',
+          marginBottom: '10px',
+          padding: '10px',
+          boxSizing: 'border-box'
+        }}>
+          <h3>Header Menu Pane</h3>
+        </div>
+
+        {/* Main Content Pane */}
+        <div style={{
+          flexGrow: 1,
+          border: '1px solid red',
+          padding: '10px',
+          boxSizing: 'border-box'
+        }}>
+          <h3>Main Content Pane</h3>
+        </div>
+      </div>
     </div>
   );
 };
 
-export default AppLists;
+export default AppListsPage;
