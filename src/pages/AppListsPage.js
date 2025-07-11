699726fea9cb2a01bd96b7ebad7586f9b0aae805
Alberto Melendez <amelendez.es@gmail.com>
2025-07-11 10:45:45 +0200

    New css file

--- src/pages/AppListsPage.js
+++ src/pages/AppListsPage.js
@@ -1,52 +1,26 @@
 
 import React from 'react';
+import './AppListsPage.css';
 
 const AppListsPage = () => {
   return (
-    <div style={{
-      display: 'flex',
-      height: '100vh',
-      backgroundColor: 'white',
-      color: 'black'
-    }}>
+    <div className="app-lists-page-container">
       {/* Left Menu Pane */}
-      <div style={{
-        width: '20%',
-        border: '1px solid red',
-        padding: '10px',
-        boxSizing: 'border-box'
-      }}>
+      <div className="left-menu-pane">
         <h2>Left Menu Pane</h2>
       </div>
 
       {/* Main Content Wrapper */}
-      <div style={{
-        width: '80%',
-        display: 'flex',
-        flexDirection: 'column',
-        border: '1px solid red',
-        padding: '10px',
-        boxSizing: 'border-box'
-      }}>
+      <div className="main-content-wrapper">
         {/* Header Menu Pane */}
-        <div style={{
-          height: '10%',
-          border: '1px solid red',
-          marginBottom: '10px',
-          padding: '10px',
-          boxSizing: 'border-box'
-        }}>
+        <div className="header-menu-pane">
           <h3>Header Menu Pane</h3>
         </div>
 
         {/* Main Content Pane */}
-        <div style={{
-          flexGrow: 1,
-          border: '1px solid red',
-          padding: '10px',
-          boxSizing: 'border-box'
-        }}>
+        <div className="main-content-pane">
           <h3>Main Content Pane</h3>
+          <h1>Hello World for Lists</h1>
         </div>
       </div>
     </div>
